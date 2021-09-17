5e18f13c9a714b66e3b15ffd5b0a18081b12e168
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-17 20:28:01 +0600

    addevent commit

--- src/components/AddEvents/AddEvents.js
+++ src/components/AddEvents/AddEvents.js
@@ -1,41 +1,65 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const AddEvents = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [imageURL, setImageURL] = useState(null);
+
+  const onSubmit = (data) => {
+    const eventData = {
+      name: data.name,
+      image: imageURL,
+    };
+    const url = `http://localhost:5000/addEvent`;
+
+    fetch(url, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(eventData),
+    }).then((response) => {
+      console.log(response);
+    });
+  };
 
   // b278a2d4e17062aed0996c67bedbf6d1
 
-  const handleImageUpload = event => {
-      console.log(event.target.files[0]);
+  const handleImageUpload = (event) => {
+    console.log(event.target.files[0]);
 
-      const imageUpload = new FormData();
-            imageUpload.set('key', 'b278a2d4e17062aed0996c67bedbf6d1');
-            imageUpload.append('image', event.target.files[0]);
+    const imageUpload = new FormData();
+    imageUpload.set("key", "b278a2d4e17062aed0996c67bedbf6d1");
+    imageUpload.append("image", event.target.files[0]);
 
-            axios.post('https://api.imgbb.com/1/upload', imageUpload)
-            .then(function (response) {
-              console.log(response.data.data.display_url);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-          
-  }
+    axios
+      .post("https://api.imgbb.com/1/upload", imageUpload)
+      .then(function (response) {
+        console.log(response.data.data.display_url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
       <h1>Add your awesome Event here</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-
-      <input name="example" defaultValue="test" ref={register} />
-      <br/>
-      <input name="exampleRequired" type="file" onChange={handleImageUpload} />
-      <br/>
-      <input type="submit" />
-    </form>
+        <input name="name" defaultValue="New exciting event" ref={register} />
+        <br />
+        <input
+          name="exampleRequired"
+          type="file"
+          onChange={handleImageUpload}
+        />
+        <br />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
